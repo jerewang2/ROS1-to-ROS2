440886468e9fb3575c8a9a84b56824a520259804
jerewang2 <jerewang1@gmail.com>
2025-04-22 11:16:36 -0400

    Print data per row.

--- csv2doc.py
+++ csv2doc.py
@@ -1,5 +1,6 @@
 import csv
 import pandas as pd
+import requests
 
 # def read_csv(file_path):
 #     data = []
@@ -19,4 +20,19 @@ def hello_world():
 
 def preprocess_csv(file_path):
     df = pd.read_csv(file_path)
-    print(df.head())
+    
+    for index, row in df.iterrows():
+        subsection = row['Name of Subsection']
+        ros1_link1 = row['ROS1 Permalink']
+        ros2_link2 = row['ROS2 Permalink']
+        description = row['Description']
+
+        code1 = requests.get(ros1_link1).text
+        code2 = requests.get(ros2_link2).text
+
+        print(f'Index: {index}')
+        print(f'Subsection: {subsection}')
+        print(f'Description: {description}')
+        print(f'ros1 link: {code1}')
+        print(f'ros2 link: {code2}')
+        
