8a57c7f5948d13b0cc061a4f1deb42653054e189
jerewang2 <jerewang1@gmail.com>
2025-04-22 16:50:36 -0400

    Process permalink and print snippet text.

--- csv2doc.py
+++ csv2doc.py
@@ -1,6 +1,7 @@
 import csv
 import pandas as pd
-import requests
+import requests # type: ignore
+import re
 
 # def read_csv(file_path):
 #     data = []
@@ -27,12 +28,26 @@ def preprocess_csv(file_path):
         ros2_link2 = row['ROS2 Permalink']
         description = row['Description']
 
-        code1 = requests.get(ros1_link1).text
-        code2 = requests.get(ros2_link2).text
+        ros1_text = process_permalink(ros1_link1)
+        ros2_text = process_permalink(ros2_link2)
 
-        print(f'Index: {index}')
-        print(f'Subsection: {subsection}')
-        print(f'Description: {description}')
-        print(f'ros1 link: {code1}')
-        print(f'ros2 link: {code2}')
-        
+        print(ros1_text)
+        print(ros2_text)
+
+
+def process_permalink(raw_link):
+    permalink = re.sub(r'^https://github\.com/', 'https://raw.githubusercontent.com/', permalink)
+    permalink = re.sub(r'/blob/', '/', permalink)
+
+    match = re.search(r'#L(\d+)C\d+-L(\d+)C\d+', permalink)
+    if match:
+        start = int(match.group(1))
+        end = int(match.group(2))
+
+    permalink = re.sub(r'#L\d+C\d+-L\d+C\d+', '', permalink)
+
+    lines_of_code = requests.get(permalink).text.splitlines()
+    snippet = lines_of_code[start - 1:end]
+    snippet_text = "\n".join(snippet)
+
+    return snippet_text
