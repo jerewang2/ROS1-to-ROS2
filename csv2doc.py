613b87b2d2462666fee96174ff59b298ea6b868c
jerewang2 <jerewang1@gmail.com>
2025-04-24 10:18:12 -0400

    More debugging print statements. Suspect request module does not work.

--- csv2doc.py
+++ csv2doc.py
@@ -44,11 +44,16 @@ def process_permalink(permalink):
         start = int(match.group(1))
         end = int(match.group(2))
 
+        print(f'Start: {start}, End: {end}')
+
         permalink = re.sub(r'#L\d+C\d+-L\d+C\d+', '', permalink)
 
         print(f'New permalink: {permalink}')
 
         lines_of_code = requests.get(permalink).text.splitlines()
+
+        print(f'Lines of code: {lines_of_code}')
+        
         snippet = lines_of_code[start - 1:end]
         snippet_text = "\n".join(snippet)
 
