1ebf6ba107c32d631cfe4e9d9ab531331d075072
jerewang2 <jerewang1@gmail.com>
2025-05-01 11:52:54 -0400

    Remove returns in snippet text.

--- csv2doc.py
+++ csv2doc.py
@@ -77,8 +77,6 @@ def process_permalink(permalink):
 
         print(f'Snippet: {snippet_text}')
 
-        snippet_text = '''1 #include <dynamic_reconfigure/server.h>
-        2 #include <geometry_msgs/PoseStamped.h>
-        3 #include <kr_mav_controllers/SO3Config.h>'''
+        snippet_text = '1 #include <dynamic_reconfigure/server.h>2 #include <geometry_msgs/PoseStamped.h>3 #include <kr_mav_controllers/SO3Config.h>'
 
         return snippet_text
